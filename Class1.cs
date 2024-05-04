4803fe8e5303d12216b396cbb9efb3bb9feacd0c
AlexPerazzo <theodoreperazzo@byui.edu>
2024-05-04 11:52:59 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		public float CalculatePoints(float weight, int playtime, float placementPercentage)
+		{
+			float weightFactor = .25 * weight + .75;
+			float playtimeFactor = ((-0.00487789 * (Math.Pow(playtime, 2))) + (2.02538 * playtime) - (1.95751)) / 100;
+			float placementFactor = placementPercentage;
+
+			float points = 10 * weightFactor * playtimeFactor * placementFactor;
+			return points;
+		}
+
+
+
+
+	}
+}
---
+++ Ultimate_Game_Winner/BottomButtons.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ultimate_Game_Winner
+{
+    /// <summary>
+    /// Interaction logic for BottomButtons.xaml
+    /// </summary>
+    public partial class BottomButtons : UserControl
+    {
+        public BottomButtons()
+        {
+            InitializeComponent();                  
+        }
+        private void Recordbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Recordbtn.Content = "Testing";
+        }
+
+        private void Leaderboardbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Leaderboardbtn.Content = "Testing2";
+        }
+
+        private void Logbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Recordbtn.Content = "Get tricked.";
+        }
+    }
+}
